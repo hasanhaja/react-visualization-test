bc3d1d2744b1a6c3e86f5cb3725b952c17f306a5
Hasan Ali <hasanhaja@gmail.com>
2021-12-27 00:27:17 +0400

    Migrate workspace to NX 13

---
+++ libs/api-parser/src/lib/api-parser.spec.ts
@@ -0,0 +1,7 @@
+import { apiParser } from './api-parser';
+
+describe('apiParser', () => {
+  it('should work', () => {
+    expect(apiParser()).toEqual('api-parser');
+  });
+});
---
+++ libs/api-parser/src/lib/api-parser.ts
@@ -0,0 +1,3 @@
+export function apiParser(): string {
+  return 'api-parser';
+}
